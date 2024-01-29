6b5bc2ef49e00b4120b03029011cf9debab4db4d
MinhNtNeu <130037696+MinhNtNeu@users.noreply.github.com>
2024-01-29 21:24:56 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/Student//Home/home_page_first.dart';
+import 'package:untitled/Student/Home/home_page_second.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const HomePageSecond(),
     );
   }
 }
